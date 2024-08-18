eb8ebbb408fccbecf96f25a1650548ea6ca6fd45
Meem Fatema <meemfatema95@gmail.com>
2024-08-17 21:11:43 -0400

    setup env

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require('express');
-const app = express;
+const app = express();
 const port = process.env.PORT || 5000;
+const cors = require("cors")
+require('dotenv').config()
+
+//middlewares
+app.use(cors());
+app.use(express.json())
 
 
 
 
 
 
-
-app.get('/', (req, res)=>{
-    res.send("Product Sevver Is Running")
+app.get('/', (req, res) => {
+    res.send("Product Server Is Running")
 })
 app.listen(port,() => {
     console.log(`Server is running on port: ${port}`)
